27a1755492d2239df6bd16c3f6e7bfc98abae2ff
Luis Calderari <luiscalderari@gmail.com>
2020-01-05 12:22:47 -0300

    Criado View/Admin

--- index.php
+++ index.php
@@ -10,8 +10,15 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+
     $page = new Page();
+    $page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+    $page = new \Hcode\PageAdmin();
     $page->setTpl("index");
 
 });
